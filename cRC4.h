4a025d78821992bf10b0b6b4538adf1de4227252
Aekras1a <aekras1a@myprivat.email>
2019-05-16 13:57:14 +1000

    First communihit
    Initiliaze README, mein commandant.

---
+++ cRC4.h
@@ -0,0 +1,25 @@
+/***********************************************************
+	* Standard RC4 algorithm
+	* class in C++
+	* Written by Viotto - BreakingSecurity.net
+***********************************************************/
+
+#include <string>
+using namespace std;
+
+class CRC4
+{
+public:
+
+	CRC4(unsigned char* pKey, unsigned int lenKey);
+	CRC4();
+
+	void RC4(unsigned char pData[], unsigned long lenData);
+	string RC4Str(unsigned char* pInputData, unsigned long InputSize);
+	void Initialize(unsigned char* pKey, unsigned int lenKey);
+
+private:
+	int m_sBox[256]; //substitution-box
+	int a, b;
+	unsigned char swap;
+};
